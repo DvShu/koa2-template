f7bae3dc0b048c1acaace7a567beba0ebe6ce078
haoran.shu <haoran.shu@laifusi.com>
2018-12-10 19:44:59 +0800

    初始上传工程
    Signed-off-by: haoran.shu <haoran.shu@laifusi.com>

---
+++ app.js
@@ -0,0 +1,41 @@
+"use strict";
+const app = new (require('koa'))();
+const logger = require('log4js').getLogger('app');
+const Router = require('koa-router');
+const serve = require('koa-static-cache');
+const log = require('koa-log4js-base');
+log.configure('{{ name }}');
+const bodyParser = require('koa-bodyparser');
+{{#if (session === 'y')}}
+const session = require('koa-session');
+{{/if }}
+{{if (template === 'y')}}
+const path = require('path');
+const render = require('koa-art-template');
+
+// 模板引擎
+render(app, {
+  root: path.join(__dirname, 'views'),
+  extname: '.html',
+  debug: true
+});
+{{/if}}
+{{if (session === 'y') }}
+// session
+app.keys = ["{{ name }}-{{ time }}"];
+app.use(session({
+  key: "{{ name }}:sess", // cookie key
+  maxAge: 1800000, // session 有效期: 30(min)
+  renew: true // 保持持久状态
+}, app)); // 配置 session
+{{/if}}
+app.use(log.init()); // 日志记录
+app.use(serve('./public',{gzip:true})); // 静态资源
+app.use(bodyParser()); // post 请求参数
+
+// 路由
+const appRouter = new Router();
+appRouter.use('/{{ name }}', require('./routes/index').routes());
+app.use(appRouter.routes()).use(appRouter.allowedMethods());
+
+module.exports = app;
