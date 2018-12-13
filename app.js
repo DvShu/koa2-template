b63f3cb2176718e2307e872bb68138edd9e8f678
haoran.shu <haoran.shu@laifusi.com>
2018-12-13 14:36:17 +0800

    重新更新模板
    Signed-off-by: haoran.shu <haoran.shu@laifusi.com>

--- app.js
+++ app.js
@@ -3,13 +3,9 @@ const app = new (require('koa'))();
 const logger = require('log4js').getLogger('app');
 const Router = require('koa-router');
 const serve = require('koa-static-cache');
-const log = require('koa-log4js-base');
-log.configure('{{ name }}');
-const bodyParser = require('koa-bodyparser');
-{{if (session === 'y')}}
-const session = require('koa-session');
-{{/if }}
-{{if (template === 'y')}}
+const logAdapter = new (require('koa-log4js-base'))('test');
+const bodyParser = require('koa-bodyparser');{{if (session === 'y')}}
+const session = require('koa-session');{{/if }}{{if (template === 'y')}}
 const path = require('path');
 const render = require('koa-art-template');
 
@@ -18,8 +14,7 @@ render(app, {
   root: path.join(__dirname, 'views'),
   extname: '.html',
   debug: true
-});
-{{/if}}
+});{{/if}}
 {{if (session === 'y') }}
 // session
 app.keys = ["{{ name }}-{{ time }}"];
@@ -29,7 +24,7 @@ app.use(session({
   renew: true // 保持持久状态
 }, app)); // 配置 session
 {{/if}}
-app.use(log.init()); // 日志记录
+app.use(logAdapter.adapt()); // 日志记录
 app.use(serve('./public',{gzip:true})); // 静态资源
 app.use(bodyParser()); // post 请求参数
 
